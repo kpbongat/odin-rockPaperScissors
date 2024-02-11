98429688b298453af3f18380931699a7061856b3
kpbongat <kpbongat.dev@gmail.com>
2024-02-11 14:36:08 +0800

    Add function for calculating a single round

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const minCeiled = Math.ceil(1);
     const maxFloored = Math.floor(3);
-    const randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
+    const randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
     switch(randomNumber) {
         case 1:
             return "Rock";
@@ -11,4 +11,25 @@ function getComputerChoice() {
             return "Scissors";
     }
   }
-  
+
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    }
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win!";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win!";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win!";
+    }
+    else {
+        return "You lose."
+    }
+  }
+    
+  playerSelection = prompt("Enter rock, paper, or scissors to play!");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
