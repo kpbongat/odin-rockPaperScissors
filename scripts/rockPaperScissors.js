1756335a6ac2f4ff90ed05cb7e31d09455391286
kpbongat <kpbongat.dev@gmail.com>
2024-03-01 17:29:48 +0800

    Add references to player selection buttons

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -52,4 +52,8 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
 playGame()
